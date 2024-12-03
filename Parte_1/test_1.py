08993af21f4e3b858b9efd6d107af2228f574a9f
Matias Abraham Rea <87036967+immlacrea@users.noreply.github.com>
2024-12-03 00:52:51 -0300

    Update test_1.py

--- Parte_1/test_1.py
+++ Parte_1/test_1.py
@@ -1,5 +1,5 @@
 import unittest
-from Ejercicio_1 import extract_coins_data_from_file, play_the_game
+from sophia_greedy import extract_coins_data_from_file, greedy_strategy
 
 winner_sophia = "Sophia"
 
@@ -9,71 +9,71 @@ class TESTEjercicio1(unittest.TestCase):
     def test_20_elements(self):
         data = "20.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
 
     def test_25_elements(self):
         data = "25.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
 
     def test_50_elements(self):
         data = "50.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
 
     def test_100_elements(self):
         data = "100.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
 
     def test_1mil_elements(self):
         data = "1000.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
 
     def test_20mil_elements(self):
         data = "20000.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
 
     def test_10mil_elements(self):
         data = "10000.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
         
     def test_15mil_elements(self):
         data = "15000.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
     
     def test_16mil_elements(self):
         data = "16000.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
         
     def test_17mil_elements(self):
         data = "17000.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
     
     def test_18mil_elements(self):
         data = "18000.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
     
     def test_19mil_elements(self):
         data = "19000.txt"
         coins = extract_coins_data_from_file(data)
-        winner = play_the_game(coins)
-        self.assertEqual(winner, winner_sophia)
+        sophia_coins, mateo_coins = greedy_strategy(coins)
+        self.assertTrue(sum(sophia_coins) > sum(mateo_coins) )
