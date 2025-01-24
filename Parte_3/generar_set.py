914d40b11b11a67f0c938c289cbf2d0c96b98d96
LucasAraujo639 <lucas.ezequiel.321@gmail.com>
2025-01-23 23:53:24 -0300

    Correcion: se agrego el grafico pedido de aproximaciones

--- Parte_3/generar_set.py
+++ Parte_3/generar_set.py
@@ -1,8 +1,12 @@
 import random
 import aproximacion_greedy as greedy
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
-MIN_TABLEROS = 5
-MAX_TABLEROS = 10
+
+MIN_TABLEROS = 1
+MAX_TABLEROS = 301
 
 def agregar_barcos_aletoriamente(barcos, n):
     cantidad_barcos = 3 * n
@@ -65,7 +69,7 @@ def generar_matriz_con_barcos(n, barcos):
         colocado = False
         intentos = 0
         #Intentamos 100 veces colocar el barco en una posicion aletoria
-        while not colocado and intentos < 1000:
+        while not colocado and intentos < 10000:
             fila = random.randint(0, n - 1)
             columna = random.randint(0, n - 1)
             horizontal = random.choice([True, False])
@@ -74,6 +78,7 @@ def generar_matriz_con_barcos(n, barcos):
                 barcos_colocados.append(longitud_barco)
                 colocado = True
             intentos += 1
+        
 
     return matriz, restricciones_fila, restricciones_columna, barcos_colocados
 
@@ -100,20 +105,21 @@ def calcular_cota_tablero(n):
     barcos = agregar_barcos_aletoriamente(barcos_colocados.copy(), n)
     random.shuffle(barcos)
     
-    solucion_greedy = greedy.aprox_sea_battle(restricciones_fila, restricciones_columna, barcos)
+    table, demandas_filas_no_cumplidas, demandas_columnas_no_cumplidas = greedy.aprox_sea_battle(restricciones_fila, restricciones_columna, barcos)
     print("\nLeyenda del tablero",n,"x",n,"para la solucion greedy:")
     print("0: Casilla valida")
     print("1: Casilla invalida")
     print("2: Barco")
 
     print("Solucion greedy del tablero:")  
-    for row in solucion_greedy[0]:
+    for row in table:
         print(row)
-    print("Restricciones que no se pudieron cubir por fila",solucion_greedy[1] )
-    print("Restricciones que no se pudieron cubir por columna",solucion_greedy[2] )
+    print("Restricciones que no se pudieron cubir por fila",demandas_filas_no_cumplidas )
+    print("Restricciones que no se pudieron cubir por columna",demandas_columnas_no_cumplidas)
+    print(n)
 
-    demandas_no_cumplidas_greedy = sum(solucion_greedy[1:3])
-    total_demandas_cumplidas_greedy = optimo_demandas_cumplidas - demandas_no_cumplidas_greedy
+    demandas_totales_no_cumplidas = demandas_filas_no_cumplidas + demandas_columnas_no_cumplidas
+    total_demandas_cumplidas_greedy = optimo_demandas_cumplidas - demandas_totales_no_cumplidas
     print("\n--------------------------------------------------\n")
     return total_demandas_cumplidas_greedy / optimo_demandas_cumplidas
 
@@ -122,6 +128,27 @@ peor_cota = min(cotas)
 
 print(f"\nLa peor cota es: {peor_cota}")
     
+
+n_values = list(range(MIN_TABLEROS, MAX_TABLEROS))
+
+# Crear el gráfico
+plt.figure(figsize=(10, 6))
+plt.plot(n_values, cotas, marker='o', linestyle='-', color='b', label='Cota obtenida')
+plt.title('Cota obtenida para cada tamaño de tablero')
+plt.xlabel('Tamaño del tablero (n)')
+plt.ylabel('Cota obtenida')
+plt.grid(True, linestyle='--', alpha=0.6)
+min_index = cotas.index(min(cotas))
+min_tick = n_values[min_index]
+ticks = [n_values[0], n_values[len(n_values) // 2], n_values[-1], min_tick]
+plt.scatter([min_tick], [min(cotas)], color='red', label='Cota mínima', zorder=5) #resaltar el minimo
+plt.xticks(ticks, labels=[str(t) for t in ticks])
+plt.xticks(n_values)
+plt.legend()
+plt.show() 
+
+
+
 
 
 
